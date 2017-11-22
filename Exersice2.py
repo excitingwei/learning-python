db6ce861db78edf62975cadbd12e5c05f55bd37e
shoplinewilliam <william@shoplineapp.com>
2017-11-22 22:37:59 +0800

    Add exercise 2

---
+++ Exersice2.py
@@ -0,0 +1,6 @@
+# A comment, this is so you can read your program later
+# Anthing after the # is ignored by python. 
+
+print("I'd love to code like this.") #我喜歡這樣子寫程式，在#之後的註解都會被忽略。
+
+print("this will show up!")
